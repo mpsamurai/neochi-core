4ebc734429a3640c69db0b6999f5cc2686d1ba82
chezyn <9652497+chezyn@users.noreply.github.com>
2019-04-28 13:26:50 +0900

    [add] defined classes of data and notification

--- src/neochi/core/dataflow/notifications/ir_sender.py
+++ src/neochi/core/dataflow/notifications/ir_sender.py
@@ -19,3 +19,16 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+from neochi.core.dataflow.notifications.base import BaseNotification
+from neochi.core.dataflow.data.base import Int
+
+
+class StartIrSendingNotification(BaseNotification):
+    data_type_cls = Int
+    channel = 'start_ir_sending'
+
+
+class CompleteIrSendingNotification(BaseNotification):
+    data_type_cls = Int
+    channel = 'complete_ir_sending'
