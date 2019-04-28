dd8d8c8f41a335cd0964605173b39b6e162328f5
TranThai <anhthai.tran@citynow.jp>
2019-04-28 13:23:05 +0900

    Add clap-detector state data

--- src/neochi/core/dataflow/data/clap_detector.py
+++ src/neochi/core/dataflow/data/clap_detector.py
@@ -19,3 +19,10 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+from neochi.core.dataflow.data import base
+from neochi.core.dataflow import data_types
+
+class State(base.BaseData):
+    data_type_cls = data_types.Str
+    key = "clap-detector:state"
--- src/neochi/core/dataflow/data/kinesis.py
+++
@@ -1,6 +0,0 @@
-from neochi.core.dataflow.data import base
-from neochi.core.dataflow import data_types
-
-class ActionPlan(base.BaseData):
-    data_type_cls = data_types.Json
-    key = 'kinesis:action-plan'
