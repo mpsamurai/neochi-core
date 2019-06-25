f300fb8f7dff1713923d086a13309862ce828d31
junyakaneko <junya@mpsamurai.org>
2019-06-26 08:37:27 +0900

    modified an import statement.

--- src/neochi/core/dataflow/data/__init__.py
+++ src/neochi/core/dataflow/data/__init__.py
@@ -20,4 +20,4 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from . import *
+from . import brain, clap_detector, eye, ir_receiver, ir_sender, kinesis
